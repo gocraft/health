fc58fdbfaf1c0b81edebfcf77de6b561d1e94979
Jonathan Novak <jnovak@gmail.com>
2014-04-03 10:07:57 -0700

    Start to plug in the health thing

--- health.go
+++ health.go
@@ -12,9 +12,6 @@ type Kvs map[string]string
 type EventReceiver interface {
 	Event(eventName string)
 	EventKv(eventName string, kvs map[string]string)
-}
-
-type TimingReceiver interface {
 	Timing(eventName string, nanoseconds int64)
 	TimingKv(eventName string, nanoseconds int64, kvs map[string]string)
 }
