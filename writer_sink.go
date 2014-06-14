9c184becc653e9a68d2a114c9aa6d32d94fb6313
Jonathan Novak <jnovak@gmail.com>
2014-06-14 13:56:11 -0700

    Touch up comment

--- writer_sink.go
+++ writer_sink.go
@@ -10,13 +10,13 @@ import (
 
 // This sink writes bytes in a format that a human might like to read in a logfile
 // This can be used to log to Stdout:
-//   .AddLogfileWriterSink(os.Stdout)
+//   .AddWriterSink(os.Stdout)
 // And to a file:
 //   f, err := os.OpenFile(fname, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-//   .AddLogfileWriterSink(f)
+//   .AddWriterSink(f)
 // And to syslog:
-//   w, err := syslog.New(LOG_INFO, "")
-//   .AddLogfileWriterSink(w)
+//   w, err := syslog.New(LOG_INFO, "wat")
+//   .AddWriterSink(w)
 type WriterSink struct {
 	io.Writer
 }
