ef8b6ee71de534f0d4c36d423c2acf1bf09191bf
Michael Nikitochkin <miry.sof@gmail.com>
2015-05-27 17:39:12 +0200

    Added Prometheus Sink

---
+++ sinks/prometheus/sink.go
@@ -0,0 +1,151 @@
+package prometheus
+
+import (
+	"time"
+
+	"github.com/gocraft/health"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+type Config struct {
+	// The Endpoint to notify about crashes. This defaults to
+	// "https://notify.bugsnag.com/", if you're using Bugsnag Enterprise then
+	// set it to your internal Bugsnag endpoint.
+	Endpoint string
+
+	// The hostname of the current server. This defaults to the return value of
+	// os.Hostname() and is graphed in the Bugsnag dashboard.
+	Hostname string
+
+	// Job name
+	Job string
+
+	// Interval push
+	Interval time.Duration
+
+	// Prometheus CountersVec. key is built by `job + event`. Required for predefined Counters.
+	CounterVecs map[string]*prometheus.CounterVec
+	HistogramVecs map[string]*prometheus.HistogramVec
+}
+
+// This sink emits to a Prometheus pushgateway daemon
+type Sink struct {
+	*Config
+	doneFlag bool
+}
+
+func NewSink(config *Config) *Sink {
+
+	if config.Endpoint == "" {
+		config.Endpoint = "http://localhost:9091/"
+	}
+
+	if config.Job == "" {
+		config.Job = "app"
+	}
+
+	if config.Interval == 0 {
+		config.Interval = time.Minute
+	}
+
+	if config.CounterVecs == nil {
+		config.CounterVecs = map[string]*prometheus.CounterVec{}
+	}
+
+	if config.HistogramVecs == nil {
+		config.HistogramVecs = map[string]*prometheus.HistogramVec{}
+	}
+
+	s := &Sink{Config: config, doneFlag: false}
+
+	go processingLoop(s)
+
+	return s
+}
+
+func (s *Sink) EmitEvent(job string, event string, kvs map[string]string) {
+	var counter *prometheus.CounterVec
+	if c, ok := s.Config.CounterVecs[job]; ok {
+		counter = c
+	} else {
+		labels := labelsFromMap(kvs)
+
+		counter = prometheus.NewCounterVec(prometheus.CounterOpts{
+			Namespace: s.Config.Job,
+			Subsystem: "indexer",
+			Name:      job,
+			Help:      "Automaticaly created event",
+		}, labels)
+
+		s.Config.CounterVecs[job] = counter
+		prometheus.Register(counter)
+	}
+
+	kvs["event"] = event
+
+	if m, err := counter.GetMetricWith(kvs); err == nil {
+		m.Inc()
+	}
+}
+
+func (s *Sink) EmitEventErr(job string, event string, inputErr error, kvs map[string]string) {
+	// no-op
+}
+
+func (s *Sink) EmitTiming(job string, event string, nanos int64, kvs map[string]string) {
+	s.emitHistogram(job, event, nanos, kvs)
+}
+
+func (s *Sink) EmitComplete(job string, status health.CompletionStatus, nanos int64, kvs map[string]string) {
+	s.emitHistogram(job, status.String(), nanos, kvs)
+}
+
+func (s *Sink) ShutdownServer() {
+	s.doneFlag = true
+}
+
+// Push current metrics each minute
+func processingLoop(sink *Sink) {
+	c := time.Tick(sink.Config.Interval)
+	for _ = range c {
+		prometheus.Push(sink.Config.Job, sink.Config.Hostname, sink.Config.Endpoint)
+		if sink.doneFlag {
+			return
+		}
+	}
+}
+
+func (s *Sink) emitHistogram(job string, event string, nanos int64, kvs map[string]string) {
+	var counter *prometheus.HistogramVec
+	if c, ok := s.Config.HistogramVecs[job]; ok {
+		counter = c
+	} else {
+		labels := labelsFromMap(kvs)
+
+		counter = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+			Namespace: s.Config.Job,
+			Subsystem: "duration",
+			Name:      job,
+			Help:      "Automaticaly created event",
+		}, labels)
+
+		s.Config.HistogramVecs[job] = counter
+		prometheus.Register(counter)
+	}
+
+	kvs["event"] = event
+
+	if m, err := counter.GetMetricWith(kvs); err == nil {
+		m.Observe(float64(nanos))
+	}
+}
+
+func labelsFromMap(kvs map[string]string) []string {
+	result := make([]string, 0, len(kvs)+1)
+	result = append(result, "event")
+	for k := range kvs {
+		result = append(result, k)
+	}
+	return result
+}
---
+++ sinks/prometheus/sink_test.go
@@ -0,0 +1,106 @@
+package prometheus
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+	"time"
+	"io/ioutil"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/gocraft/health"
+)
+
+func TestSinkEmitEvent(t *testing.T) {
+	config := &Config{
+		Endpoint: "http://localhost:9091",
+		Job:      "app",
+		Hostname: "localhost",
+		Interval: time.Second,
+	}
+
+	s := NewSink(config)
+	defer s.ShutdownServer()
+
+	h := prometheusHandler{
+		PayloadChan: make(chan *payload),
+	}
+
+	s.EmitEvent("thejob", "theevent", map[string]string{"handler": "/vary/url/path/to/worldnice"})
+
+	go http.ListenAndServe(":9091", h)
+
+	p := <-h.PayloadChan
+	assert.Equal(t, p.Url, "/metrics/jobs/app/instances/localhost")
+	assert.Contains(t, string(p.Body), "theevent")
+	assert.Contains(t, string(p.Body), "thejob")
+}
+
+func TestSinkEmitTiming(t *testing.T) {
+	config := &Config{
+		Endpoint: "http://localhost:9092",
+		Job:      "app",
+		Hostname: "localhost",
+		Interval: time.Second,
+	}
+
+	s := NewSink(config)
+	defer s.ShutdownServer()
+
+	h := prometheusHandler{
+		PayloadChan: make(chan *payload),
+	}
+
+	go http.ListenAndServe(":9092", h)
+
+	s.EmitTiming("thetimejob", "theevent", 10, map[string]string{"handler": "/vary/url/path/to/worldnice"})
+
+	p := <-h.PayloadChan
+	assert.Equal(t, p.Url, "/metrics/jobs/app/instances/localhost")
+	assert.Contains(t, string(p.Body), "theevent")
+	assert.Contains(t, string(p.Body), "thetimejob")
+	assert.Contains(t, string(p.Body), "handler")
+}
+
+func TestSinkEmitComplete(t *testing.T) {
+	config := &Config{
+		Endpoint: "http://localhost:9093",
+		Job:      "app",
+		Hostname: "localhost",
+		Interval: time.Second,
+	}
+
+	s := NewSink(config)
+	defer s.ShutdownServer()
+
+	h := prometheusHandler{
+		PayloadChan: make(chan *payload),
+	}
+
+	go http.ListenAndServe(":9093", h)
+
+	s.EmitComplete("thecomplete", health.Success, 10, map[string]string{"handler": "/oops"})
+
+	p := <-h.PayloadChan
+	assert.Equal(t, p.Url, "/metrics/jobs/app/instances/localhost")
+	assert.Contains(t, string(p.Body), "thecomplete")
+	assert.Contains(t, string(p.Body), "handler")
+}
+
+
+type payload struct {
+	Url string
+	Hostname string
+	Body []byte
+}
+
+type prometheusHandler struct {
+	PayloadChan chan *payload
+}
+
+func (h prometheusHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	body, _ := ioutil.ReadAll(r.Body)
+	resp := payload{Url: r.URL.Path, Body: body}
+	h.PayloadChan <- &resp
+	fmt.Fprintf(rw, "OK")
+}
