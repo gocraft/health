fa3194f62eb949a2bfb80f46e5bff6bba274ebe7
lnmx <len@lnmx.org>
2015-06-10 12:28:58 -0400

    fix goroutine shutdown

--- sinks/bugsnag/sink.go
+++ sinks/bugsnag/sink.go
@@ -71,10 +71,11 @@ func errorProcessingLoop(sink *Sink) {
 	cmdChan := sink.cmdChan
 	doneChan := sink.doneChan
 
+PROCESSING_LOOP:
 	for {
 		select {
 		case <-doneChan:
-			break
+			break PROCESSING_LOOP
 		case cmd := <-cmdChan:
 			if err := Notify(sink.Config, cmd.Job, cmd.Event, cmd.Err, cmd.Err.Stack); err != nil {
 				fmt.Fprintf(os.Stderr, "bugsnag.Notify: could not notify bugsnag. err=%v\n", err)
