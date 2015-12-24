b2e9538a81c4c183f0d8bb5437bac2916cc56b21
Jonathan Novak <jnovak@gmail.com>
2015-12-23 21:34:25 -0800

    FEATURE: high perf, zero alloc statsd sink
    Re-wrote the statsd sink to be about 10x faster and generate zero garbage. It uses the multiline feature of statsd to buffer 1440 bytes into 1 UDP packet instead of sending a new one each time. It also avoids tons of allocations by caching sanitizations and key prefixes. This implementation is also marginally faster than a reference implementation at github.com/mailgun/metrics.

--- statsd_sink.go
+++ statsd_sink.go
@@ -2,165 +2,365 @@ package health
 
 import (
 	"bytes"
-	"fmt"
 	"net"
+	"strconv"
 	"time"
 )
 
-type StatsDSinkSantizationFunc func(string) string
+type StatsDSinkSanitizationFunc func(*bytes.Buffer, string)
 
-// This sink emits to a StatsD deaemon by sending it a UDP packet.
-type StatsDSink struct {
-	SanitizationFunc StatsDSinkSantizationFunc
+type eventKey struct {
+	job    string
+	event  string
+	suffix string
+}
 
-	conn net.Conn
+type prefixBuffer struct {
+	*bytes.Buffer
+	prefixLen int
+}
 
+type StatsDSinkOptions struct {
 	// Prefix is something like "metroid"
 	// Events emitted to StatsD would be metroid.myevent.wat
 	// Eg, don't include a trailing dot in the prefix.
 	// It can be "", that's fine.
-	prefix string
+	Prefix string
+
+	// SanitizationFunc sanitizes jobs and events before sending them to statsd
+	SanitizationFunc StatsDSinkSanitizationFunc
+
+	// SkipNestedEvents will skip {events,timers,gauges} from sending the job.event version
+	// and will only send the event version.
+	SkipNestedEvents bool
+
+	// SkipTopLevelEvents will skip {events,timers,gauges} from sending the event version
+	// and will only send the job.event version.
+	SkipTopLevelEvents bool
 }
 
-func NewStatsDSink(addr, prefix string) (Sink, error) {
-	c, err := net.DialTimeout("udp", addr, 2*time.Second)
+var defaultStatsDOptions = StatsDSinkOptions{SanitizationFunc: sanitizeKey}
+
+type StatsDSink struct {
+	options StatsDSinkOptions
+
+	cmdChan       chan statsdEmitCmd
+	drainDoneChan chan struct{}
+	stopDoneChan  chan struct{}
+
+	flushPeriod time.Duration
+
+	udpBuf    bytes.Buffer
+	timingBuf []byte
+
+	udpConn *net.UDPConn
+	udpAddr *net.UDPAddr
+
+	// map of {job,event,suffix} to a re-usable buffer prefixed with the key.
+	// Since each timing/gauge has a unique component (the time), we'll truncate to the prefix, write the timing,
+	// and write the statsD suffix (eg, "|ms\n"). Then copy that to the UDP buffer.
+	prefixBuffers map[eventKey]prefixBuffer
+}
+
+type statsdCmdKind int
+
+const (
+	statsdCmdKindEvent statsdCmdKind = iota
+	statsdCmdKindEventErr
+	statsdCmdKindTiming
+	statsdCmdKindGauge
+	statsdCmdKindComplete
+	statsdCmdKindFlush
+	statsdCmdKindDrain
+	statsdCmdKindStop
+)
+
+type statsdEmitCmd struct {
+	Kind   statsdCmdKind
+	Job    string
+	Event  string
+	Nanos  int64
+	Value  float64
+	Status CompletionStatus
+}
+
+const cmdChanBuffSize = 8192 // random-ass-guess
+const maxUdpBytes = 1440     // 1500(Ethernet MTU) - 60(Max UDP header size
+
+func NewStatsDSink(addr string, options *StatsDSinkOptions) (*StatsDSink, error) {
+	c, err := net.ListenPacket("udp", ":0")
 	if err != nil {
 		return nil, err
 	}
 
-	sink := &StatsDSink{
-		SanitizationFunc: sanitizeKey,
-		conn:             c,
-		prefix:           prefix,
+	ra, err := net.ResolveUDPAddr("udp", addr)
+	if err != nil {
+		return nil, err
 	}
 
-	return sink, nil
+	s := &StatsDSink{
+		udpConn:       c.(*net.UDPConn),
+		udpAddr:       ra,
+		cmdChan:       make(chan statsdEmitCmd, cmdChanBuffSize),
+		drainDoneChan: make(chan struct{}),
+		stopDoneChan:  make(chan struct{}),
+		flushPeriod:   100 * time.Millisecond,
+		prefixBuffers: make(map[eventKey]prefixBuffer),
+	}
+
+	if options != nil {
+		s.options = *options
+		if s.options.SanitizationFunc == nil {
+			s.options.SanitizationFunc = sanitizeKey
+		}
+	} else {
+		s.options = defaultStatsDOptions
+	}
+
+	go s.loop()
+
+	return s, nil
 }
 
-// If event is "my.event", and job is "cool.job"
-// This will emit two events to statsd:
-// "my.event"
-// "cool.job.my.event"
-// (this will apply prefix if it's set, so "prefix.my.event" and "prefix.cool.job.my.event")
+func (s *StatsDSink) Stop() {
+	s.cmdChan <- statsdEmitCmd{Kind: statsdCmdKindStop}
+	<-s.stopDoneChan
+}
+
+func (s *StatsDSink) Drain() {
+	s.cmdChan <- statsdEmitCmd{Kind: statsdCmdKindDrain}
+	<-s.drainDoneChan
+}
+
 func (s *StatsDSink) EmitEvent(job string, event string, kvs map[string]string) {
-	key1, key2 := s.eventKeys(job, event, "")
-	s.inc(key1)
-	s.inc(key2)
+	s.cmdChan <- statsdEmitCmd{Kind: statsdCmdKindEvent, Job: job, Event: event}
 }
 
-// If event is "my.event", and job is "cool.job",
-// This will emit two events: "my.event.error" and "cool.job.my.event.error" (prefix applied if present)
 func (s *StatsDSink) EmitEventErr(job string, event string, inputErr error, kvs map[string]string) {
-	key1, key2 := s.eventKeys(job, event, "error")
-	s.inc(key1)
-	s.inc(key2)
+	s.cmdChan <- statsdEmitCmd{Kind: statsdCmdKindEventErr, Job: job, Event: event}
 }
 
 func (s *StatsDSink) EmitTiming(job string, event string, nanos int64, kvs map[string]string) {
-	key1, key2 := s.eventKeys(job, event, "")
-	s.measure(key1, nanos)
-	s.measure(key2, nanos)
+	s.cmdChan <- statsdEmitCmd{Kind: statsdCmdKindTiming, Job: job, Event: event, Nanos: nanos}
 }
 
 func (s *StatsDSink) EmitGauge(job string, event string, value float64, kvs map[string]string) {
-	key1, key2 := s.eventKeys(job, event, "")
-	s.gauge(key1, value)
-	s.gauge(key2, value)
+	s.cmdChan <- statsdEmitCmd{Kind: statsdCmdKindGauge, Job: job, Event: event, Value: value}
 }
 
-// if job is "my.job", this will emit "my.job.xyz" where xyz is "success", etc (see completionStatusToString)
 func (s *StatsDSink) EmitComplete(job string, status CompletionStatus, nanos int64, kvs map[string]string) {
-	var b bytes.Buffer
-
-	if s.prefix != "" {
-		b.WriteString(s.prefix)
-		b.WriteRune('.')
-	}
-	b.WriteString(s.SanitizationFunc(job))
-	b.WriteRune('.')
-	b.WriteString(status.String())
-
-	s.measure(b.String(), nanos)
+	s.cmdChan <- statsdEmitCmd{Kind: statsdCmdKindComplete, Job: job, Status: status, Nanos: nanos}
 }
 
-func (s *StatsDSink) eventKeys(job, event, suffix string) (string, string) {
-	var key1 bytes.Buffer // event
-	var key2 bytes.Buffer // job.event
+func (s *StatsDSink) loop() {
+	cmdChan := s.cmdChan
 
-	if s.prefix != "" {
-		key1.WriteString(s.prefix)
-		key1.WriteRune('.')
-		key2.WriteString(s.prefix)
-		key2.WriteRune('.')
-	}
+	ticker := time.NewTicker(s.flushPeriod)
+	go func() {
+		for _ = range ticker.C {
+			cmdChan <- statsdEmitCmd{Kind: statsdCmdKindFlush}
+		}
+	}()
 
-	key1.WriteString(s.SanitizationFunc(event))
-	key2.WriteString(s.SanitizationFunc(job))
-	key2.WriteRune('.')
-	key2.WriteString(s.SanitizationFunc(event))
-
-	if suffix != "" {
-		key1.WriteRune('.')
-		key1.WriteString(suffix)
-		key2.WriteRune('.')
-		key2.WriteString(suffix)
-	}
-
-	return key1.String(), key2.String()
-}
-
-func (s *StatsDSink) inc(key string) {
-	var msg bytes.Buffer
-	msg.WriteString(key)
-	msg.WriteString(":1|c\n")
-	s.send(msg.Bytes())
-}
-
-func (s *StatsDSink) measure(key string, nanos int64) {
-	var msg bytes.Buffer
-	msg.WriteString(key)
-	msg.WriteRune(':')
-	msg.WriteString(fmt.Sprintf("%f", float64(nanos)/float64(time.Millisecond)))
-	msg.WriteString("|ms\n")
-	s.send(msg.Bytes())
-}
-
-func (s *StatsDSink) gauge(key string, value float64) {
-	var msg bytes.Buffer
-	msg.WriteString(key)
-	msg.WriteRune(':')
-	fmt.Fprintf(&msg, "%g", value)
-	msg.WriteString("|g\n")
-	s.send(msg.Bytes())
-}
-
-func (s *StatsDSink) send(msg []byte) {
-	s.conn.Write(msg)
-}
-
-func shouldSanitize(r rune) bool {
-	switch r {
-	case '|', ':':
-		return true
-	}
-	return false
-}
-
-func sanitizeKey(k string) string {
-	for _, r := range k {
-		if shouldSanitize(r) {
-			goto SANITIZE
+LOOP:
+	for cmd := range cmdChan {
+		switch cmd.Kind {
+		case statsdCmdKindDrain:
+		DRAIN_LOOP:
+			for {
+				select {
+				case cmd := <-cmdChan:
+					s.processCmd(&cmd)
+				default:
+					s.flush()
+					s.drainDoneChan <- struct{}{}
+					break DRAIN_LOOP
+				}
+			}
+		case statsdCmdKindStop:
+			s.stopDoneChan <- struct{}{}
+			break LOOP
+		case statsdCmdKindFlush:
+			s.flush()
+		default:
+			s.processCmd(&cmd)
 		}
 	}
-	return k
-SANITIZE:
-	var key bytes.Buffer
-	for _, r := range k {
-		if shouldSanitize(r) {
-			key.WriteRune('$')
+
+	ticker.Stop()
+}
+
+func (s *StatsDSink) processCmd(cmd *statsdEmitCmd) {
+	switch cmd.Kind {
+	case statsdCmdKindEvent:
+		s.processEvent(cmd.Job, cmd.Event)
+	case statsdCmdKindEventErr:
+		s.processEventErr(cmd.Job, cmd.Event)
+	case statsdCmdKindTiming:
+		s.processTiming(cmd.Job, cmd.Event, cmd.Nanos)
+	case statsdCmdKindGauge:
+		s.processGauge(cmd.Job, cmd.Event, cmd.Value)
+	case statsdCmdKindComplete:
+		s.processComplete(cmd.Job, cmd.Status, cmd.Nanos)
+	}
+}
+
+func (s *StatsDSink) processEvent(job string, event string) {
+	if !s.options.SkipTopLevelEvents {
+		pb := s.getPrefixBuffer("", event, "")
+		pb.WriteString("1|c\n")
+		s.writeStatsDMetric(pb.Bytes())
+	}
+
+	if !s.options.SkipNestedEvents {
+		pb := s.getPrefixBuffer(job, event, "")
+		pb.WriteString("1|c\n")
+		s.writeStatsDMetric(pb.Bytes())
+	}
+}
+
+func (s *StatsDSink) processEventErr(job string, event string) {
+	if !s.options.SkipTopLevelEvents {
+		pb := s.getPrefixBuffer("", event, "error")
+		pb.WriteString("1|c\n")
+		s.writeStatsDMetric(pb.Bytes())
+	}
+
+	if !s.options.SkipNestedEvents {
+		pb := s.getPrefixBuffer(job, event, "error")
+		pb.WriteString("1|c\n")
+		s.writeStatsDMetric(pb.Bytes())
+	}
+}
+
+func (s *StatsDSink) processTiming(job string, event string, nanos int64) {
+	s.writeNanosToTimingBuf(nanos)
+
+	if !s.options.SkipTopLevelEvents {
+		pb := s.getPrefixBuffer("", event, "")
+		pb.Write(s.timingBuf)
+		pb.WriteString("|ms\n")
+		s.writeStatsDMetric(pb.Bytes())
+	}
+
+	if !s.options.SkipNestedEvents {
+		pb := s.getPrefixBuffer(job, event, "")
+		pb.Write(s.timingBuf)
+		pb.WriteString("|ms\n")
+		s.writeStatsDMetric(pb.Bytes())
+	}
+}
+
+func (s *StatsDSink) processGauge(job string, event string, value float64) {
+	s.timingBuf = s.timingBuf[0:0]
+	s.timingBuf = strconv.AppendFloat(s.timingBuf, value, 'f', 2, 64)
+
+	if !s.options.SkipTopLevelEvents {
+		pb := s.getPrefixBuffer("", event, "")
+		pb.Write(s.timingBuf)
+		pb.WriteString("|g\n")
+		s.writeStatsDMetric(pb.Bytes())
+	}
+
+	if !s.options.SkipNestedEvents {
+		pb := s.getPrefixBuffer(job, event, "")
+		pb.Write(s.timingBuf)
+		pb.WriteString("|g\n")
+		s.writeStatsDMetric(pb.Bytes())
+	}
+}
+
+func (s *StatsDSink) processComplete(job string, status CompletionStatus, nanos int64) {
+	s.writeNanosToTimingBuf(nanos)
+	statusString := status.String()
+
+	pb := s.getPrefixBuffer(job, "", statusString)
+	pb.Write(s.timingBuf)
+	pb.WriteString("|ms\n")
+	s.writeStatsDMetric(pb.Bytes())
+}
+
+func (s *StatsDSink) flush() {
+	if s.udpBuf.Len() > 0 {
+		s.udpConn.WriteToUDP(s.udpBuf.Bytes(), s.udpAddr)
+		s.udpBuf.Truncate(0)
+	}
+}
+
+// assumes b is a well-formed statsd metric like "job.event:1|c\n" (including newline)
+func (s *StatsDSink) writeStatsDMetric(b []byte) {
+	lenb := len(b)
+
+	if lenb == 0 {
+		return
+	}
+
+	// single metric exceeds limit. sad day.
+	if lenb > maxUdpBytes {
+		return
+	}
+
+	lenUdpBuf := s.udpBuf.Len()
+
+	if (lenb + lenUdpBuf) > maxUdpBytes {
+		s.udpConn.WriteToUDP(s.udpBuf.Bytes(), s.udpAddr)
+		s.udpBuf.Truncate(0)
+	}
+
+	s.udpBuf.Write(b)
+}
+
+func (s *StatsDSink) getPrefixBuffer(job, event, suffix string) prefixBuffer {
+	key := eventKey{job, event, suffix}
+
+	b, ok := s.prefixBuffers[key]
+	if !ok {
+		b.Buffer = &bytes.Buffer{}
+		s.writeSanitizedKeys(b.Buffer, s.options.Prefix, job, event, suffix)
+		b.WriteByte(':')
+		b.prefixLen = b.Len()
+
+		// 123456789.99|ms\n 16 bytes. timing value represents 11 days max
+		b.Grow(16)
+		s.prefixBuffers[key] = b
+	} else {
+		b.Truncate(b.prefixLen)
+	}
+
+	return b
+}
+
+func (s *StatsDSink) writeSanitizedKeys(b *bytes.Buffer, keys ...string) {
+	needDot := false
+	for _, k := range keys {
+		if k != "" {
+			if needDot {
+				b.WriteByte('.')
+			}
+			s.options.SanitizationFunc(b, k)
+			needDot = true
+		}
+	}
+}
+
+func (s *StatsDSink) writeNanosToTimingBuf(nanos int64) {
+	s.timingBuf = s.timingBuf[0:0]
+	if nanos >= 10e6 {
+		// More than 10 milliseconds. We'll just print as an integer
+		s.timingBuf = strconv.AppendInt(s.timingBuf, nanos/1e6, 10)
+	} else {
+		s.timingBuf = strconv.AppendFloat(s.timingBuf, float64(nanos)/float64(time.Millisecond), 'f', 2, 64)
+	}
+}
+
+func sanitizeKey(b *bytes.Buffer, s string) {
+	b.Grow(len(s) + 1)
+	for i := 0; i < len(s); i++ {
+		si := s[i]
+		if ('A' <= si && si <= 'Z') || ('a' <= si && si <= 'z') || ('0' <= si && s[i] <= '9') || si == '_' || si == '.' {
+			b.WriteByte(si)
 		} else {
-			key.WriteRune(r)
+			b.WriteByte('$')
 		}
 	}
-	return key.String()
 }
--- statsd_sink_test.go
+++ statsd_sink_test.go
@@ -1,13 +1,12 @@
 package health
 
 import (
-	// "bytes"
-	// "errors"
 	"fmt"
 	"github.com/stretchr/testify/assert"
 	"net"
 	"runtime"
 	"strings"
+	"sync"
 	"testing"
 	"time"
 )
@@ -31,9 +30,9 @@ func listenFor(t *testing.T, msgs []string, f func()) {
 
 	f()
 
-	buf := make([]byte, 1024)
+	buf := make([]byte, 10000)
 	for _, msg := range msgs {
-		err = c.SetReadDeadline(time.Now().Add(1 * time.Second))
+		err = c.SetReadDeadline(time.Now().Add(1 * time.Millisecond))
 		assert.NoError(t, err)
 		nbytes, _, err := c.ReadFrom(buf)
 		assert.NoError(t, err)
@@ -46,104 +45,335 @@ func listenFor(t *testing.T, msgs []string, f func()) {
 	}
 }
 
-func TestStatsDSinkEmitEventPrefix(t *testing.T) {
-	sink, err := NewStatsDSink(testAddr, "metroid")
+func TestStatsDSinkPeriodicPurge(t *testing.T) {
+	sink, err := NewStatsDSink(testAddr, &StatsDSinkOptions{Prefix: "metroid"})
 	assert.NoError(t, err)
-	listenFor(t, []string{"metroid.my.event:1|c\n", "metroid.my.job.my.event:1|c\n"}, func() {
+
+	// Stop the sink, set a smaller flush period, and start it agian
+	sink.Stop()
+	sink.flushPeriod = 1 * time.Millisecond
+	go sink.loop()
+	defer sink.Stop()
+
+	listenFor(t, []string{"metroid.my.event:1|c\nmetroid.my.job.my.event:1|c\n"}, func() {
 		sink.EmitEvent("my.job", "my.event", nil)
+		time.Sleep(10 * time.Millisecond)
+	})
+}
+
+func TestStatsDSinkPacketLimit(t *testing.T) {
+	sink, err := NewStatsDSink(testAddr, &StatsDSinkOptions{Prefix: "metroid", SkipNestedEvents: true})
+	assert.NoError(t, err)
+
+	// s is 101 bytes
+	s := "metroid." + strings.Repeat("a", 88) + ":1|c\n"
+
+	// expect 1 packet that is 14*101=1414 bytes, and the next one to be 101 bytes
+	listenFor(t, []string{strings.Repeat(s, 14), s}, func() {
+		for i := 0; i < 15; i++ {
+			sink.EmitEvent("my.job", strings.Repeat("a", 88), nil)
+		}
+
+		sink.Drain()
+	})
+}
+
+func TestStatsDSinkEmitEventPrefix(t *testing.T) {
+	sink, err := NewStatsDSink(testAddr, &StatsDSinkOptions{Prefix: "metroid"})
+	defer sink.Stop()
+	assert.NoError(t, err)
+	listenFor(t, []string{"metroid.my.event:1|c\nmetroid.my.job.my.event:1|c\n"}, func() {
+		sink.EmitEvent("my.job", "my.event", nil)
+		sink.Drain()
 	})
 }
 
 func TestStatsDSinkEmitEventShouldSanitize(t *testing.T) {
-	sink, err := NewStatsDSink(testAddr, "metroid")
+	sink, err := NewStatsDSink(testAddr, &StatsDSinkOptions{Prefix: "metroid"})
+	defer sink.Stop()
 	assert.NoError(t, err)
-	listenFor(t, []string{"metroid.my$event:1|c\n", "metroid.my$job.my$event:1|c\n"}, func() {
+	listenFor(t, []string{"metroid.my$event:1|c\nmetroid.my$job.my$event:1|c\n"}, func() {
 		sink.EmitEvent("my|job", "my:event", nil)
+		sink.Drain()
 	})
 }
 
 func TestStatsDSinkEmitEventNoPrefix(t *testing.T) {
-	sink, err := NewStatsDSink(testAddr, "")
+	sink, err := NewStatsDSink(testAddr, nil)
+	defer sink.Stop()
 	assert.NoError(t, err)
-	listenFor(t, []string{"my.event:1|c\n", "my.job.my.event:1|c\n"}, func() {
+	listenFor(t, []string{"my.event:1|c\nmy.job.my.event:1|c\n"}, func() {
 		sink.EmitEvent("my.job", "my.event", nil)
+		sink.Drain()
+	})
+}
+
+func TestStatsDSinkEmitEventSkipNested(t *testing.T) {
+	sink, err := NewStatsDSink(testAddr, &StatsDSinkOptions{SkipNestedEvents: true})
+	defer sink.Stop()
+	assert.NoError(t, err)
+	listenFor(t, []string{"my.event:1|c\n"}, func() {
+		sink.EmitEvent("my.job", "my.event", nil)
+		sink.Drain()
+	})
+}
+
+func TestStatsDSinkEmitEventSkipTopLevel(t *testing.T) {
+	sink, err := NewStatsDSink(testAddr, &StatsDSinkOptions{SkipTopLevelEvents: true})
+	defer sink.Stop()
+	assert.NoError(t, err)
+	listenFor(t, []string{"my.job.my.event:1|c\n"}, func() {
+		sink.EmitEvent("my.job", "my.event", nil)
+		sink.Drain()
 	})
 }
 
 func TestStatsDSinkEmitEventErrPrefix(t *testing.T) {
-	sink, err := NewStatsDSink(testAddr, "metroid")
+	sink, err := NewStatsDSink(testAddr, &StatsDSinkOptions{Prefix: "metroid"})
+	defer sink.Stop()
 	assert.NoError(t, err)
-	listenFor(t, []string{"metroid.my.event.error:1|c\n", "metroid.my.job.my.event.error:1|c\n"}, func() {
+	listenFor(t, []string{"metroid.my.event.error:1|c\nmetroid.my.job.my.event.error:1|c\n"}, func() {
 		sink.EmitEventErr("my.job", "my.event", testErr, nil)
+		sink.Drain()
 	})
 }
 
 func TestStatsDSinkEmitEventErrNoPrefix(t *testing.T) {
-	sink, err := NewStatsDSink(testAddr, "")
+	sink, err := NewStatsDSink(testAddr, nil)
+	defer sink.Stop()
 	assert.NoError(t, err)
-	listenFor(t, []string{"my.event.error:1|c\n", "my.job.my.event.error:1|c\n"}, func() {
+	listenFor(t, []string{"my.event.error:1|c\nmy.job.my.event.error:1|c\n"}, func() {
 		sink.EmitEventErr("my.job", "my.event", testErr, nil)
+		sink.Drain()
+	})
+}
+
+func TestStatsDSinkEmitEventErrSkipNested(t *testing.T) {
+	sink, err := NewStatsDSink(testAddr, &StatsDSinkOptions{SkipNestedEvents: true})
+	defer sink.Stop()
+	assert.NoError(t, err)
+	listenFor(t, []string{"my.event.error:1|c\n"}, func() {
+		sink.EmitEventErr("my.job", "my.event", testErr, nil)
+		sink.Drain()
+	})
+}
+
+func TestStatsDSinkEmitEventErrSkipTopLevel(t *testing.T) {
+	sink, err := NewStatsDSink(testAddr, &StatsDSinkOptions{SkipTopLevelEvents: true})
+	defer sink.Stop()
+	assert.NoError(t, err)
+	listenFor(t, []string{"my.job.my.event.error:1|c\n"}, func() {
+		sink.EmitEventErr("my.job", "my.event", testErr, nil)
+		sink.Drain()
 	})
 }
 
 func TestStatsDSinkEmitTimingPrefix(t *testing.T) {
-	sink, err := NewStatsDSink(testAddr, "metroid")
+	sink, err := NewStatsDSink(testAddr, &StatsDSinkOptions{Prefix: "metroid"})
+	defer sink.Stop()
 	assert.NoError(t, err)
-	listenFor(t, []string{"metroid.my.event:123.456789|ms\n", "metroid.my.job.my.event:123.456789|ms\n"}, func() {
+	listenFor(t, []string{"metroid.my.event:123|ms\nmetroid.my.job.my.event:123|ms\n"}, func() {
 		sink.EmitTiming("my.job", "my.event", 123456789, nil)
+		sink.Drain()
 	})
 }
 
 func TestStatsDSinkEmitTimingNoPrefix(t *testing.T) {
-	sink, err := NewStatsDSink(testAddr, "")
+	sink, err := NewStatsDSink(testAddr, nil)
+	defer sink.Stop()
 	assert.NoError(t, err)
-	listenFor(t, []string{"my.event:123.456789|ms\n", "my.job.my.event:123.456789|ms\n"}, func() {
+	listenFor(t, []string{"my.event:123|ms\nmy.job.my.event:123|ms\n"}, func() {
 		sink.EmitTiming("my.job", "my.event", 123456789, nil)
+		sink.Drain()
+	})
+}
+
+func TestStatsDSinkEmitTimingSkipNested(t *testing.T) {
+	sink, err := NewStatsDSink(testAddr, &StatsDSinkOptions{SkipNestedEvents: true})
+	defer sink.Stop()
+	assert.NoError(t, err)
+	listenFor(t, []string{"my.event:123|ms\n"}, func() {
+		sink.EmitTiming("my.job", "my.event", 123456789, nil)
+		sink.Drain()
+	})
+}
+
+func TestStatsDSinkEmitTimingSkipTopLevel(t *testing.T) {
+	sink, err := NewStatsDSink(testAddr, &StatsDSinkOptions{SkipTopLevelEvents: true})
+	defer sink.Stop()
+	assert.NoError(t, err)
+	listenFor(t, []string{"my.job.my.event:123|ms\n"}, func() {
+		sink.EmitTiming("my.job", "my.event", 123456789, nil)
+		sink.Drain()
+	})
+}
+
+func TestStatsDSinkEmitTimingShort(t *testing.T) {
+	sink, err := NewStatsDSink(testAddr, nil)
+	defer sink.Stop()
+	assert.NoError(t, err)
+	listenFor(t, []string{"my.event:1.23|ms\nmy.job.my.event:1.23|ms\n"}, func() {
+		sink.EmitTiming("my.job", "my.event", 1234567, nil)
+		sink.Drain()
 	})
 }
 
 func TestStatsDSinkEmitGaugePrefix(t *testing.T) {
-	sink, err := NewStatsDSink(testAddr, "metroid")
+	sink, err := NewStatsDSink(testAddr, &StatsDSinkOptions{Prefix: "metroid"})
+	defer sink.Stop()
 	assert.NoError(t, err)
-	listenFor(t, []string{"metroid.my.event:3.14|g\n", "metroid.my.job.my.event:3.14|g\n"}, func() {
+	listenFor(t, []string{"metroid.my.event:3.14|g\nmetroid.my.job.my.event:3.14|g\n"}, func() {
 		sink.EmitGauge("my.job", "my.event", 3.14, nil)
+		sink.Drain()
 	})
 }
 
 func TestStatsDSinkEmitGaugeNoPrefix(t *testing.T) {
-	sink, err := NewStatsDSink(testAddr, "")
+	sink, err := NewStatsDSink(testAddr, nil)
+	defer sink.Stop()
 	assert.NoError(t, err)
-	listenFor(t, []string{"my.event:3|g\n", "my.job.my.event:3|g\n"}, func() {
+	listenFor(t, []string{"my.event:3.00|g\nmy.job.my.event:3.00|g\n"}, func() {
 		sink.EmitGauge("my.job", "my.event", 3, nil)
+		sink.Drain()
+	})
+}
+
+func TestStatsDSinkEmitGaugeSkipNested(t *testing.T) {
+	sink, err := NewStatsDSink(testAddr, &StatsDSinkOptions{SkipNestedEvents: true})
+	defer sink.Stop()
+	assert.NoError(t, err)
+	listenFor(t, []string{"my.event:3.00|g\n"}, func() {
+		sink.EmitGauge("my.job", "my.event", 3, nil)
+		sink.Drain()
+	})
+}
+
+func TestStatsDSinkEmitGaugeSkipTopLevel(t *testing.T) {
+	sink, err := NewStatsDSink(testAddr, &StatsDSinkOptions{SkipTopLevelEvents: true})
+	defer sink.Stop()
+	assert.NoError(t, err)
+	listenFor(t, []string{"my.job.my.event:3.00|g\n"}, func() {
+		sink.EmitGauge("my.job", "my.event", 3, nil)
+		sink.Drain()
 	})
 }
 
 func TestStatsDSinkEmitCompletePrefix(t *testing.T) {
-	sink, err := NewStatsDSink(testAddr, "metroid")
+	sink, err := NewStatsDSink(testAddr, &StatsDSinkOptions{Prefix: "metroid"})
+	defer sink.Stop()
 	assert.NoError(t, err)
 	for kind, kindStr := range completionStatusToString {
-		str := fmt.Sprintf("metroid.my.job.%s:129.456789|ms\n", kindStr)
+		str := fmt.Sprintf("metroid.my.job.%s:129|ms\n", kindStr)
 		listenFor(t, []string{str}, func() {
 			sink.EmitComplete("my.job", kind, 129456789, nil)
+			sink.Drain()
 		})
 	}
 }
 
 func TestStatsDSinkEmitCompleteNoPrefix(t *testing.T) {
-	sink, err := NewStatsDSink(testAddr, "")
+	sink, err := NewStatsDSink(testAddr, nil)
+	defer sink.Stop()
 	assert.NoError(t, err)
 	for kind, kindStr := range completionStatusToString {
-		str := fmt.Sprintf("my.job.%s:129.456789|ms\n", kindStr)
+		str := fmt.Sprintf("my.job.%s:129|ms\n", kindStr)
 		listenFor(t, []string{str}, func() {
 			sink.EmitComplete("my.job", kind, 129456789, nil)
+			sink.Drain()
 		})
 	}
 }
 
 func TestStatsDSinkEmitTimingSubMillisecond(t *testing.T) {
-	sink, err := NewStatsDSink(testAddr, "metroid")
+	sink, err := NewStatsDSink(testAddr, &StatsDSinkOptions{Prefix: "metroid"})
+	defer sink.Stop()
 	assert.NoError(t, err)
-	listenFor(t, []string{"metroid.my.event:0.456789|ms\n", "metroid.my.job.my.event:0.456789|ms\n"}, func() {
+	listenFor(t, []string{"metroid.my.event:0.46|ms\nmetroid.my.job.my.event:0.46|ms\n"}, func() {
 		sink.EmitTiming("my.job", "my.event", 456789, nil)
+		sink.Drain()
 	})
+}
+
+func BenchmarkStatsDSinkProcessEvent(b *testing.B) {
+	sink, _ := NewStatsDSink(testAddr, &StatsDSinkOptions{Prefix: "metroid"})
+	sink.Stop() // Don't do periodic things while we're benching
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		sink.processEvent("myjob", "myevent")
+	}
+}
+
+func BenchmarkStatsDSinkProcessEventErr(b *testing.B) {
+	sink, _ := NewStatsDSink(testAddr, &StatsDSinkOptions{Prefix: "metroid"})
+	sink.Stop() // Don't do periodic things while we're benching
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		sink.processEventErr("myjob", "myevent")
+	}
+}
+
+func BenchmarkStatsDSinkProcessTimingBig(b *testing.B) {
+	sink, _ := NewStatsDSink(testAddr, &StatsDSinkOptions{Prefix: "metroid"})
+	sink.Stop() // Don't do periodic things while we're benching
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		sink.processTiming("myjob", "myevent", 30000000)
+	}
+}
+
+func BenchmarkStatsDSinkProcessTimingSmall(b *testing.B) {
+	sink, _ := NewStatsDSink(testAddr, &StatsDSinkOptions{Prefix: "metroid"})
+	sink.Stop() // Don't do periodic things while we're benching
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		sink.processTiming("myjob", "myevent", 1230000)
+	}
+}
+
+func BenchmarkStatsDSinkProcessGauge(b *testing.B) {
+	sink, _ := NewStatsDSink(testAddr, &StatsDSinkOptions{Prefix: "metroid"})
+	sink.Stop() // Don't do periodic things while we're benching
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		sink.processGauge("myjob", "myevent", 3.14)
+	}
+}
+
+func BenchmarkStatsDSinkProcessComplete(b *testing.B) {
+	sink, _ := NewStatsDSink(testAddr, &StatsDSinkOptions{Prefix: "metroid"})
+	sink.Stop() // Don't do periodic things while we're benching
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		sink.processComplete("myjob", Success, 1230000)
+	}
+}
+
+func BenchmarkStatsDSinkOverall(b *testing.B) {
+	const numGoroutines = 100
+	var requestsPerGoroutine = b.N / numGoroutines
+
+	stream := NewStream()
+	sink, _ := NewStatsDSink(testAddr, &StatsDSinkOptions{Prefix: "metroid"})
+	stream.AddSink(sink)
+	job := stream.NewJob("foo")
+
+	wg := sync.WaitGroup{}
+	for i := 0; i < numGoroutines; i++ {
+		wg.Add(1)
+		go func() {
+			for j := 0; j < requestsPerGoroutine; j++ {
+				job.Event("evt")
+			}
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+	sink.Drain()
 }
